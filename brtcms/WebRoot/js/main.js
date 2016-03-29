81bbac43ce9aa8dc5513cc81a1f1404aeee37790
buruite <xyblovo2008@sina.com>
2016-03-30 01:10:09 +0800

    修改个人中心、登录、注册

--- brtcms/WebRoot/js/main.js
+++ brtcms/WebRoot/js/main.js
@@ -7,11 +7,40 @@ $(function(){
 	}).on("mouseout",function(){
 		$(this).css({"color":"#ffffff"});
 	});
-	$(".popover").on("mouseover",function(){
-		alert(222);
-		//$(this).css({"color":"#ffffff","background-color":"#4AA3DB"});
+	//显示个人信息
+	var popovermsg = "1";
+	$(".person_popover").on("mouseenter",function(){
+		if(popovermsg == "1"){
+			$('.button_person').popover('show');
+		}
+	}).on("mouseleave",function(){
+		if(popovermsg == "2"){
+			$('.button_person').popover('hide');
+		}
 	});
 	$('[data-toggle="popover"]').popover();
+	$('.navbar_top_right').on('hidden.bs.popover', function () {
+		popovermsg = "1";
+	}).on("shown.bs.popover", function () {
+		popovermsg = "2";
+	});
+	/*切换手机或邮箱*/
+	$("#res_tab li").on("click",function(){
+		if($(this).attr("class") == undefined || $(this).attr("class") == ""){
+			$("#res_tab li").each(function(){
+				$(this).removeClass("active");
+			});
+			if($(this).find("a").html().trim() == "邮箱注册"){
+				$("#pb_res_phone").hide();
+				$("#pb_res_email").show();
+			}else if($(this).find("a").html().trim() == "手机注册"){
+				$("#pb_res_email").hide();
+				$("#pb_res_phone").show();
+			};
+			$(this).addClass("active");
+		}
+	});
+	/*回到顶部*/
 	$(window).scroll(function(){  
         if ($(window).scrollTop()>500){  
             $(".back_to_top").fadeIn(500);  
@@ -27,7 +56,14 @@ $(function(){
         return false;  
     });
 });
-
+/*登录*/
+function login(){
+	$("#loginModal").modal("show");
+}
+/*注册*/
+function register(){
+	$("#registerModal").modal("show");
+}
 function person(){
 	alert("我的主页");
 }
